48a50fb098161878cac603c504c7c7dcce496837
tintedsky <hongbonew@gmail.com>
2018-03-18 19:22:09 -0400

    1. npmjs.com 2. an example on how to use third party modules 3. put 'node_modules/' into .gitignore file 4. before we use 'npm install' to let other guys to download the module you use. 5. use 'npm init' command

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js')
 
-console.log(`Result: ${notes.add(9, -2)}`);
+console.log(_.isString(true));
+console.log(_.isString('Muyang!'));
+console.log(_.isString(1));
 
+var filteredArray = _.uniq(['Hong', 1, 'Hong', 1, 2, 3, 4]);
+console.log(filteredArray);
 // var user = os.userInfo();
 //
 // fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function(err){
