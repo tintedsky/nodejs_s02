5c912f09a094ca6194a737cd9101ea9f39985a52
tintedsky <hongbonew@gmail.com>
2018-03-18 16:19:44 -0400

    basic function example

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js')
 
-var user = os.userInfo();
+var res = notes.addNote();
+console.log(res);
 
-fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function(err){
-  if (err){
-    console.log('Unable to write to file.')
-  }
-});
+// var user = os.userInfo();
+//
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function(err){
+//   if (err){
+//     console.log('Unable to write to file.')
+//   }
+// });
--- notes.js
+++ notes.js
@@ -1,3 +1,6 @@
 console.log('Starting notes.js')
 
-module.exports.age=25;
+module.exports.addNote = () => {
+  console.log('addNote');
+  return 'New note';
+};
