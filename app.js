2e6e6b1d6d86b7e848f5fdf0084dbf9ae1e9d634
tintedsky <hongbonew@gmail.com>
2018-03-18 15:09:46 -0400

    l009 Version 2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const os = require('os');
 
 var user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, function(err){
+fs.appendFile('greetings.txt', 'Hello ' + user.username+'!', function(err){
   if (err){
     console.log('Unable to write to file.')
   }
