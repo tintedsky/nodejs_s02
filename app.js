ac5508b3ed3f55b7a9f5eb1923d98d0ce71eedb0
tintedsky <hongbonew@gmail.com>
2018-03-18 23:03:50 -0400

    node app.js jhlkjhl. --title "sec 2" node app.js jhlkjhl. --title="sec 2"

--- app.js
+++ app.js
@@ -5,4 +5,19 @@ const _ = require('lodash');
 
 const notes = require('./notes.js');
 
+var command = process.argv[2];
+console.log('Command: ', command);
+
+if (command === 'add'){
+  console.log('Adding new note');
+} else if (command === 'list'){
+  console.log('List all notes');
+} else if (command === 'read'){
+  console.log('data fetched from the server');
+} else if (command === 'remove'){
+  console.log('note removed.');
+}else{
+  console.log('Command not recognized.');
+}
+
 console.log(process.argv);
