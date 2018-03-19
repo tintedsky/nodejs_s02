f10cd2d0635d4966f9e3ce911a865a9584fcb991
tintedsky <hongbonew@gmail.com>
2018-03-18 22:10:54 -0400

    fresh start for lecture 13

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
-const notes = require('./notes.js')
 
-console.log(_.isString(true));
-console.log(_.isString('Muyang!'));
-console.log(_.isString(1));
-
-var filteredArray = _.uniq(['Hong']);
-console.log(filteredArray);
-// var user = os.userInfo();
-//
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function(err){
-//   if (err){
-//     console.log('Unable to write to file.')
-//   }
-// });
+const notes = require('./notes.js');
