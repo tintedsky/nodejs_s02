6af09f7f1364bcdeb70b3e4a86e9b91eb1e2c127
tintedsky <hongbonew@gmail.com>
2018-03-21 19:09:15 -0400

    JSON: object saved in file

--- playground/json.js
+++ playground/json.js
@@ -1,12 +1,24 @@
-// var obj = {
-//   name: 'Moose'
-// };
-//
-// var stringObj = JSON.stringify(obj);
-// console.log(typeof stringObj);
-// console.log(stringObj
 
-var personString = '{"name":"Moose", "age": 25}';
-var person = JSON.parse(personString); //convert to object
-console.log(typeof person);
-console.log(person);
+// Coding plan
+
+// get a third party fs
+const fs = require('fs');
+
+// create a note object
+// convert the object to string
+var note = {
+  title: 'some title',
+  body: 'some body'
+};
+var originalNoteString = JSON.stringify(note);
+
+// save the string to a file
+// fetch the string from the file
+fs.writeFileSync('notes.json', originalNoteString);
+var fetchedNoteString = fs.readFileSync('notes.json');
+
+// convert the string to an object
+// print the type and content
+var convertedNoteObj = JSON.parse(fetchedNoteString);
+console.log(typeof convertedNoteObj);
+console.log(convertedNoteObj.title);
