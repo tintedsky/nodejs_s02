3fdaa1e40a46d034a090d86a0d8e6b2733bc00df
tintedsky <hongbonew@gmail.com>
2018-03-21 06:52:25 -0400

    JSON: string to object

--- playground/json.js
+++ playground/json.js
@@ -1,7 +1,12 @@
-var obj = {
-  name: 'Moose'
-};
+// var obj = {
+//   name: 'Moose'
+// };
+//
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj
 
-var stringObj = JSON.stringify(obj);
-console.log(typeof stringObj);
-console.log(stringObj);
+var personString = '{"name":"Moose", "age": 25}';
+var person = JSON.parse(personString); //convert to object
+console.log(typeof person);
+console.log(person);
