f1dc7517133810b2967d3c7ea58942d53b45f7af
tintedsky <hongbonew@gmail.com>
2018-03-21 22:08:04 -0400

    1. Save the json string to file; 2. Using try-catch block to prevent crashing when the target file does not exit. 3. Using filter to create a call back function. As a result, only unique key-value pair can be saved to the target file.

--- notes.js
+++ notes.js
@@ -1,7 +1,29 @@
 console.log('Starting notes.js')
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-  console.log('Adding note:', title, body);
+  var notes = [];
+  var note = {
+    title,
+    body
+  };
+
+  /* Prevent crash when the file 'notes-data.json' does not exist */
+  try {
+    var stringNotes = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(stringNotes);
+  }catch (e) {}
+
+  /* filter is an array method that takes a callback function */
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  /* Rule out the duplicate notes. */
+  if (duplicateNotes.length===0){
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
+
 };
 
 var getAll = () => {
