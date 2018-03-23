c3a6638dcaa8ad0ab7955b00b2da5405633004c9
tintedsky <hongbonew@gmail.com>
2018-03-23 13:22:23 -0400

    debug code with command line

--- notes.js
+++ notes.js
@@ -54,6 +54,7 @@ var removeNote = (title) => {
 };
 
 var logNote = (note) => {
+  debugger;
   console.log('--');
   console.log(`Title: ${note.title}`);
   console.log(`Body: ${note.body}`);
---
+++ playground/debugging.js
@@ -0,0 +1,11 @@
+var person = {
+  name:"Moose"
+};
+
+debugger;
+
+person.age = 17;
+person.name = "mooooya";
+
+console.log(person.age);
+console.log(person.name);
